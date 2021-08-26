9f29d41ba88545e649c37ea30a76589f2758db9a
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-08-25 22:53:06 -0400

    Gerador de frases completo.

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,95 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main(){
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeStateful(),
+    home: Home(),
   ));
 
 }
 
-class HomeStateful extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _HomeStatefulState createState() => _HomeStatefulState();
+  _HomeState createState() => _HomeState();
 }
 
-class _HomeStatefulState extends State<HomeStateful> {
 
-  var _titulo =  "Lucca D'Amico";
+class _HomeState extends State<Home> {
+
+  var _frases = [
+    "A persistência é o caminho do êxito.",
+    "As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.",
+    "Motivação é a arte de fazer as pessoas fazerem o que você quer que elas façam porque elas o querem fazer.",
+    "Toda ação humana, quer se torne positiva ou negativa, precisa depender de motivação.",
+    "No meio da dificuldade encontra-se a oportunidade.",
+    "Lute. Acredite. Conquiste. Perca. Deseje. Espere. Alcance. Invada. Caia. Seja tudo o quiser ser, mas, acima de tudo, seja você sempre.",
+    "Eu faço da dificuldade a minha motivação. A volta por cima vem na continuação.",
+    "A verdadeira motivação vem de realização, desenvolvimento pessoal, satisfação no trabalho e reconhecimento.",
+    "Pedras no caminho? Eu guardo todas. Um dia vou construir um castelo.",
+    "É parte da cura o desejo de ser curado.",
+    "Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.",
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase motivacional!";
+
+  void _gerarFrase(){
+    var numeroSorteado = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
-
-    print("Build Chamado");
     return Scaffold(
       appBar: AppBar(
-        title: Text("Instagram"),
-        backgroundColor: Colors.greenAccent,
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
       ),
-      body: Container(
-       child: Column(
-         children: <Widget>[
-           ElevatedButton(
-               onPressed: (){
-                 setState(() {
-                   _titulo = "Curso Flutter";
-                 });
-                 print("clicado");
-               },
-               child: Text("Clique aqui!")
-           ),
-           Text("Nome: $_titulo")
-         ],
-       ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              border: Border.all(width: 3, color: Colors.amber)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/1594765315717-4.png"),
+              Text(
+                _fraseGerada,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 17,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black
+                ),
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  _gerarFrase();
+                },
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-
     );
   }
 }
